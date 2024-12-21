ce4481a4b91d987cc1be9b811b5f61c5da04c6fb
ramzan zoldik <54402804+rabouzia@users.noreply.github.com>
2024-12-21 21:19:25 +0100

    dsf

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 all: start
 
 start:
-	mkdir -p srcs/my_volume
+	mkdir -p srcs/my_volume/wordpress
+	mkdir -p srcs/my_volume/mariadb
+	mkdir -p srcs/my_volume/adminer
 	docker compose --project-directory srcs up --build -d
 
 stop:
 	docker compose --project-directory srcs down
 
+delete:
+	sudo rm -rf srcs/my_volume/wordpress
+	sudo rm -rf srcs/my_volume/mariadb
+
 logs:
 	docker compose --project-directory srcs logs
 
-.PHONY: start all logs
+.PHONY: start all logs delete
--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /var/www/html
 
+
 if [ -d "wordpress" ]; then
     echo "wordpress exists."
 else
@@ -14,25 +15,23 @@ else
     mv wp-cli.phar /usr/local/bin/wp
     wp core download --allow-root 
     
-    sleep 5
-    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --allow-root 
-    sleep 5
+    wp config create --dbname=$BDD_NAME --dbuser=$BDD_USER --dbpass=$BDD_USER_PASSWORD --dbhost=$BDD_HOST --allow-root 
 
-    wp core install --url=ybelatar.42.fr --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root  
-    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_USER_ROLE --porcelain --allow-root 
+    wp core install --url=rabouzia.42.fr --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root  
+    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --porcelain --allow-root 
     
-    wp config set WP_REDIS_HOST redis --add --allow-root
-    wp config set WP_REDIS_PORT 6379 --add --allow-root
-    wp config set WP_CACHE true --add --allow-root
-    wp plugin install contact-form-7 --activate --allow-root
-    wp plugin install redis-cache --activate --allow-root
-    wp theme delete twentynineteen twentytwenty --allow-root
-    wp plugin delete hello --allow-root
-    wp plugin update --all --allow-root  
-    wp redis enable --allow-root  
+    # wp config set WP_REDIS_HOST redis --add --allow-root
+    # wp config set WP_REDIS_PORT 6379 --add --allow-root
+    # wp config set WP_CACHE true --add --allow-root
+    # wp plugin install contact-form-7 --activate --allow-root
+    # wp plugin install redis-cache --activate --allow-root
+    # wp theme delete twentynineteen twentytwenty --allow-root
+    # wp plugin delete hello --allow-root
+    # wp plugin update --all --allow-root  
+    # wp redis enable --allow-root
     echo "END" 
 fi
 
 chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads
 
-/usr/sbin/php-fpm7.4 -F
+exec $@
