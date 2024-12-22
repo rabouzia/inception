b77e740b820375e958dea9b04d97b5e333a48f8b
Ramzy Zoldik <rabouzia@student.42.fr>
2024-12-22 13:54:08 +0100

    wordpress launches but fail creating user

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,37 +1,62 @@
-#!/bin/sh
-cd /var/www/html
+#!/bin/bash
 
+#---------------------------------------------------zsh---------------------------------------------------#
 
-if [ -d "wordpress" ]; then
-    echo "wordpress exists."
-else
-    echo "wordpress not exists." 
-    mkdir -p wordpress
-    cd wordpress
-    
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    php wp-cli.phar --info
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
-    wp core download --allow-root 
-    
-    wp config create --dbname=$BDD_NAME --dbuser=$BDD_USER --dbpass=$BDD_USER_PASSWORD --dbhost=$BDD_HOST --allow-root 
+chsh -s $(which zsh)
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sed -i '553i echo alias wp=\\"wp --allow-root --path=/var/www/html/wordpress\\" >> ~/.zshrc' /install.sh
+echo "alias zshi='sh /install.sh'" >> ~/.zshrc
 
-    wp core install --url=rabouzia.42.fr --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root  
-    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --porcelain --allow-root 
-    
-    # wp config set WP_REDIS_HOST redis --add --allow-root
-    # wp config set WP_REDIS_PORT 6379 --add --allow-root
-    # wp config set WP_CACHE true --add --allow-root
-    # wp plugin install contact-form-7 --activate --allow-root
-    # wp plugin install redis-cache --activate --allow-root
-    # wp theme delete twentynineteen twentytwenty --allow-root
-    # wp plugin delete hello --allow-root
-    # wp plugin update --all --allow-root  
-    # wp redis enable --allow-root
-    echo "END" 
+#---------------------------------------------------mariadb ping---------------------------------------------------#
+
+ping_mariadb_container() {
+    nc -zv mariadb 3306 > /dev/null
+    return $?
+}
+start_time=$(date +%s)
+end_time=$((start_time + 20))
+while [ $(date +%s) -lt $end_time ]; do
+    ping_mariadb_container
+    if [ $? -eq 0 ]; then
+        echo "[========MARIADB IS UP AND RUNNING========]"
+        break
+    else
+        echo "[========WAITING FOR MARIADB TO START...========]"
+        sleep 1
+    fi
+done
+
+if [ $(date +%s) -ge $end_time ]; then
+    echo "[========MARIADB IS NOT RESPONDING========]"
 fi
 
-chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads
+#---------------------------------------------------wp installation---------------------------------------------------#
 
-exec $@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress/
+chown -R www-data:www-data /var/www/html/wordpress
+
+check_core_files() {
+    wp core is-installed --allow-root > /dev/null
+    return $?
+}
+if ! check_core_files; then
+    echo "[========WP INSTALLATION STARTED========]"
+    find /var/www/html/wordpress/ -mindepth 1 -delete
+    wp core download --allow-root
+    wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+    wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+    wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
+else
+    echo "[========WordPress files already exist. Skipping installation========]"
+fi
+
+#---------------------------------------------------php config---------------------------------------------------#
+
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php
+/usr/sbin/php-fpm7.4 -F
