c16186c8f241b05f17e739e7b3ee49828ed831f7
ramzan zoldik <54402804+rabouzia@users.noreply.github.com>
2024-12-25 00:09:22 +0100

    starting bonus

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ start:
 	mkdir -p srcs/my_volume/wordpress
 	mkdir -p srcs/my_volume/mariadb
 	mkdir -p srcs/my_volume/adminer
+	mkdir -p srcs/my_volume/minecraft
 	docker compose --project-directory srcs up --build -d
 
 stop:
@@ -11,11 +12,14 @@ stop:
 
 delete:
 	sudo rm -rf srcs/my_volume/wordpress
-	sudo rm -rf srcs/my_volume/mariadb
+	# sudo rm -rf srcs/my_volume/mariadb
+	sudo rm -rf srcs/my_volume/minecraft
+
 supp:
 	docker volume rm srcs_db
 	docker volume rm srcs_wp
 	docker volume rm srcs_adm
+	docker volume rm srcs_mc
 
 
 logs:
--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -14,7 +14,7 @@ chown -R www-data:www-data /var/www/html/wordpress
 echo "[========WP INSTALLATION STARTED========]"
 find /var/www/html/wordpress/ -mindepth 1 -delete
 wp core download --allow-root
-wp core config --dbhost=mariadb:3006 --dbname="$BDD_NAME" --dbuser="$BDD_USER" --dbpass="$BDD_USER_PASSWORD" --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$BDD_NAME" --dbuser="$BDD_USER" --dbpass="$BDD_USER_PASSWORD" --allow-root
 wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"--allow-root
 sleep 5 
